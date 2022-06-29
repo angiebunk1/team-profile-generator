2c6bfe383cc750555622b3a80c19dd806006de84
angiebunk1 <103800228+angiebunk1@users.noreply.github.com>
2022-06-29 13:52:34 -0500

    loving the styles

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -11,22 +11,26 @@ const generateHTML = (employeeArr) => {
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <title>Employee Profiles</title>
     </head>
-    <body>
+    <body class="container-fluid bg-info">
+    <header class="text-light"><h1>Employee Profiles</h1><header>
+        <div class="row ml-2">
         ${employeeArr.map((e) => {
-            return`<section>
-            <P>Name:${e.name}</P>
-            <P>Title:${e.title}</P>
-            <p>ID:${e.id}</p>
-            <p>Email:${e.email}</p>
-            <p>${fieldTitle(e)}</p>
-            <p>${finalField(e)}</P>
-        </section>`
+            return`<div class="card ml-1" style="width: 18rem;">
+            <div class="card-body">
+            <h3 class="card-title text-success">Name: ${e.name}</h3>
+            <h4 class="card-text text-secondary">Title: ${e.title}</h4>
+            <p class="card-text text-dark">ID: ${e.id}</p>
+            <p class="card-text text-dark">Email: ${e.email}</p>
+            <p class="card-text text-dark">${fieldLabel(e)} ${finalField(e)}</p>
+            </div>
+        </div>`
         })}
+        <div>
     </body>
     </html>`
 }
 
-const fieldTitle = (e) => {
+const fieldLabel = (e) => {
     if (e.title === "Manager") {
         return "Office Number:"
     }
