e5e2fab5cc6603a179a15802197dad8326bb19c7
angiebunk1 <103800228+angiebunk1@users.noreply.github.com>
2022-06-29 10:34:45 -0500

    all test passing so far

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,31 @@
+const Employee = require('../lib/Employee.js');
+const Engineer = require('../lib/Engineer.js');
+
+test('testing for title', () => {
+    const newEngineer = new Engineer();
+
+    expect(newEngineer.title).toBe("Engineer");
+})
+
+test('testing for getRole()', () => {
+    const newEngineer = new Engineer();
+
+    expect(newEngineer.getRole()).toBe("Engineer");
+})
+
+test('testing for github', () => {
+    const testValue = 'garycodes31'
+
+    const newEngineer = new Engineer('Dave', '32', 'dave@gmail.com', testValue);
+
+    expect(newEngineer.github).toBe(testValue);
+});
+
+test('testing for getGithub()', () => {
+    const testValue = 'garycodes31'
+
+    const newEngineer = new Engineer('Dave', '32', 'dave@gmail.com', testValue);
+
+    expect(newEngineer.getGithub()).toBe(testValue);
+});
+
